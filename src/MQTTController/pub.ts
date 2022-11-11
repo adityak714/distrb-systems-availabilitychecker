5b7bdcc1e667c4334f6e8155ead49b0ec0a02a95
Kwabena Asare <kwabenaa@net.chalmers.se>
2022-11-11 16:23:41 +0100

    feat: MQTT connection skeleton

--- src/MQTTController/pub.ts
+++ src/MQTTController/pub.ts
@@ -1,15 +1,45 @@
-import mqtt from 'mqtt';
-const client = mqtt.connect('mqtt://test.mosquitto.org');
-client.on('connect', () => {
-  client.subscribe('presence', (err: Error) => {
-    if (!err) {
-      client.publish('presence', 'Hello mqtt');
-    }
-  });
-});
+// Create a client instance
+const client = new Paho.MQTT.Client('mqtt://test.mosquitto.org', Number(location.port), "clientId");
+//topic for subscribing 
+const firstTopic :string = "topic/availability/#"
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
 
-client.on('message', (topic: string, message: string) => {
-  // message is Buffer
-  console.log(message.toString());
-  client.end();
-});
+
+// called when the client connects
+const onConnect = () : void  => {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("onConnect");
+  client.subscribe(firstTopic);
+}
+
+const publishMessage = (message : string, topic: string) : void => {
+  if(!client.isConnected) {
+    console.log("Client is not connected")
+  }
+  const mqttMessage = new Paho.MQTT.Message(message)
+  mqttMessage.destinationName = topic;
+  client.send(mqttMessage)
+}
+//This function is called when the client is connected to the broker successfully
+client.connect({onSuccess:onConnect});
+// called when the client loses its connection
+function onConnectionLost(responseObject: any) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// Called when a message is received
+//This function will have to publish a response which will contain the data retrieved from the database
+function onMessageArrived(message: Paho.MQTT.Message) {
+  //check if client is connected
+  if(!client.isConnected) {
+    console.log("Client not connected")
+  }
+  if(message.destinationName === firstTopic) {
+    publishMessage("data from the database", message.destinationName)
+  }
+
+}
