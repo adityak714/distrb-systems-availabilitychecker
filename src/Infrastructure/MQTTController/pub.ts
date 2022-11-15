2062a64760abf1765cf9cc401122e46373be1375
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-11-15 16:07:37 +0100

    feat: working mqtt functionality

--- src/Infrastructure/MQTTController/pub.ts
+++ src/Infrastructure/MQTTController/pub.ts
@@ -1,41 +1,13 @@
-import mqtt, {IClientOptions, MqttClient} from 'mqtt';
-import {createAppointmentCommand} from '../../Application/Commands/createAppointmentCommand';
+/* eslint-disable prettier/prettier */
+import mqtt from 'mqtt'
+const client = mqtt.connect('mqtt://broker.hivemq.com');
 
-const host = 'broker.mqttdashboard.com'
-const topic:  string = 'topic/availability'
-
-const options : IClientOptions = {
-  port :1883,
-  clientId: 'dentistimo-mqtt',
-  reconnectPeriod: 1000
-  }
-  
-const client = mqtt.connect(host, options)
-const message: string= '{"userid": 12345, "requestid" : 13, "dentistid" : 1, "issuance": 1602406766314, "date": "2020-12-14"}';
-  
 client.on('connect', () => {
-console.log('Connected')
-  while (client.connected) {
-    client.publish(topic, message, {qos: 0}, function (err) {
-      if (err) {
-          console.log("An error occurred during publish")
-      } else {
-          console.log("Published successfully to " + topic.toString() + "with message " + message.toString())
-      }
-  });
-  }
-})
-
-client.on('error', error => {
-  console.log('Error occurred: ' + error);
-});
-
-// Notify reconnection
-client.on('reconnect', () => {
-  console.log('Reconnection starting');
-});
-
-// Notify offline status
-client.on('offline', () => {
-  console.log('Currently offline. Please check internet!');
+  setInterval(() => {
+    const random = Math.random() * 50;
+    console.log(random);
+    if (random < 30) {
+      client.publish('PradeepKumar', 'Simple MQTT using HiveMQ: ' + random.toString() + '.');
+    }
+  }),30000;
 });
