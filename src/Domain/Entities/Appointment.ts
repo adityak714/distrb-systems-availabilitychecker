8d36108a5443ab01ae35db032f24e370898a3842
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-11-17 11:05:52 +0100

    disabled prettier

--- src/Domain/Entities/Appointment.ts
+++ src/Domain/Entities/Appointment.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {IAppointment} from '../Intefaces/IAppointment';
 
 export class Appointment implements IAppointment {
@@ -7,7 +8,7 @@ export class Appointment implements IAppointment {
   issuance: number;
   date: Date;
 
-  constructor(userId: number, dentistId: number, requestId:number, issuance: number, date: Date) {
+  constructor(userId: number, dentistId: number, requestId: number, issuance: number, date: Date) {
     this.userId = userId;
     this.dentistId = dentistId;
     this.requestId = requestId;
